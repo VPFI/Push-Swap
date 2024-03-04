f2a0ebcca1444dd9547f1c751669396f3c8b86bc
Victor Perez Figueras <vperez-f@cbr8s1.42barcelona.com>
2024-03-04 16:07:50 +0100

    sorting loop first v. not correct yet

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: vperez-f <vperez-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 13:35:51 by vperez-f          #+#    #+#             */
-/*   Updated: 2024/03/01 17:36:38 by vperez-f         ###   ########.fr       */
+/*   Updated: 2024/03/04 15:59:30 by vperez-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,37 +27,43 @@ void	fill_stack(t_stack **stack, int argc, char **argv)
 		argc--;
 	}
 }
+void	print_stacks(t_stack **stack_a, t_stack **stack_b)
+{
+	int	i;
+	t_stack *temp;
+
+	i = 1;
+	temp = *stack_a;
+	while (temp)
+	{
+		if (temp->num)
+			{
+				printf("Stack A[%i]: %i\n", i, temp -> num);
+			}
+		temp = temp -> next;
+		i++;
+	}
+	i = 1;
+	temp = *stack_b;
+	while (temp)
+	{
+		if (temp -> num)
+			{
+				printf("Stack B[%i]: %i\n", i, temp -> num);
+			}
+		temp = temp->next;
+		i++;
+	}
+}
 int main(int argc, char **argv)
 {
 	t_stack	*stack_a;
 	t_stack	*stack_b;
-	int		i;
 
-	i = 1;
 	stack_a = NULL;
 	stack_b = NULL;
 	if (!argc)
 		return (1);	
 	fill_stack(&stack_a, argc, argv);
 	first_sort(&stack_a, &stack_b, argc);
-
-	while (stack_a)
-	{
-		if (stack_a->num)
-			{
-				printf("Stack A[%i]: %i\n", i, stack_a -> num);
-			}
-		stack_a = stack_a -> next;
-		i++;
-	}
-	i = 1;
-	while (stack_b)
-	{
-		if (stack_b -> num)
-			{
-				printf("Stack B[%i]: %i\n", i, stack_b -> num);
-			}
-		stack_b = stack_b->next;
-		i++;
-	}
 }
