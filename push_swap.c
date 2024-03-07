3132cb2d43f416a05f65d074825da2303e6b2273
Victor Perez Figueras <vperez-f@cbr9s4.42barcelona.com>
2024-03-07 17:59:38 +0100

    ordered stacks from start

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: vperez-f <vperez-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 13:35:51 by vperez-f          #+#    #+#             */
-/*   Updated: 2024/03/04 20:34:13 by vperez-f         ###   ########.fr       */
+/*   Updated: 2024/03/07 17:53:58 by vperez-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,56 @@ void	fill_stack(t_stack **stack, int argc, char **argv)
 {
 	//check if (s)? and NULL nodes
 	t_stack	*node;
+	t_stack	*temp;
 	int		i;
 
 	i = 1;
 	while ((argc - 1) > 0)
 	{
 		node = ft_stack_new(ft_atoi(argv[i]));
-		ft_stackadd_back(stack, node);
-		i++;
-		argc--;
+		if (stack)		
+		{
+			if (*stack)
+			{
+				temp = *stack;
+				if (node->num < (temp)->num)
+				{
+					ft_stackadd_front(stack, node);
+				}
+				else
+				{
+					while (temp->next)
+					{
+						if (node->num > temp->num && (node->num < (temp->next)->num))
+						{					
+							node->next = temp->next;
+							temp->next = node;
+						}
+						temp = temp->next;
+					}
+					if (node->num > temp->num && temp->next == NULL)
+					{
+						ft_stackadd_back(stack, node);
+					}
+				}
+			}
+			else
+			{
+				*stack = node;
+			}
+			i++;
+			argc--;
+		}
 	}
 }
+/*
+while ((argc - 1) > 0)
+{
+	node = ft_stack_new(ft_atoi(argv[i]));
+	ft_stackadd_back(stack, node);
+	i++;
+	argc--;
+}*/
 void	print_stacks(t_stack **stack_a, t_stack **stack_b)
 {
 	int	i;
@@ -42,22 +81,22 @@ void	print_stacks(t_stack **stack_a, t_stack **stack_b)
 		{
 			if (temp_a->num)
 				{
-					printf("A[%02d]|| %02d --- ", i, temp_a -> num);
+					printf("A[%03d]|| %03d --- ", i, temp_a -> num);
 				}
 			temp_a = temp_a -> next;
 		}
 		else
-			printf("A[%02d]|| \\\\ --- ", i);
+			printf("A[%03d]|| \\\\ --- ", i);
 		if (temp_b)
 		{
 			if (temp_b -> num)
 				{
-					printf("%02d ||[%02d]B\n", temp_b -> num, i);
+					printf("%03d ||[%03d]B\n", temp_b -> num, i);
 				}
 			temp_b = temp_b -> next;
 		}
 		else
-			printf("\\\\ ||[%02d]B\n", i);
+			printf("\\\\ ||[%03d]B\n", i);
 		i++;
 	}
 }
@@ -65,12 +104,15 @@ int main(int argc, char **argv)
 {
 	t_stack	*stack_a;
 	t_stack	*stack_b;
+	//t_stack	*test;
 
 	stack_a = NULL;
 	stack_b = NULL;
 	if (!argc)
 		return (1);	
 	fill_stack(&stack_a, argc, argv);
-	first_sort(&stack_a, &stack_b, argc);
+	//first_sort(&stack_a, &stack_b, argc);
 	print_stacks(&stack_a, &stack_b);
+	//test = ft_stacklast(stack_a);
+	//printf("%i\n", test->num);
 }
